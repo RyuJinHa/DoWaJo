63c975086bcb2d1a5902be109a5c8234764e5ac7
RyuJinHa <43023487+RyuJinHa@users.noreply.github.com>
2020-09-29 15:59:17 +0900

    합본

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -54,9 +54,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Hanriver = new LatLng(37.538631, 126.934323);
+        mMap.addMarker(new MarkerOptions().position(Hanriver).title("Marker in Hanriver"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Hanriver));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
 
     }
